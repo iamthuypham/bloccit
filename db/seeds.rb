7c28e7dd9ae1a37e3d7cfb37e3baf87e7a95fe65
Thuy Pham <tpham045@hotmail.com>
2016-07-03 03:42:35 +0000

    Add a unique post and a unique comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,23 @@ require 'random_data'
 
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
  
+ unique_post_data  = {
+     title: "Assignment-30 Post", 
+     body: "This is a unique post to assignment 30!"
+ }
+ unique_post = Post.find_or_create_by(unique_post_data)
+ 
+ unique_comment_data  = {
+     body: "This is a unique comment to assignment 30!"
+ }
+ 
+ unique_post.comments.find_or_create_by(unique_comment_data)
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
