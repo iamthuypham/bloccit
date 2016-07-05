5bbe0be716777518a90c27b8137d34314b2786f9
Thuy Pham <tpham045@hotmail.com>
2016-07-05 04:01:46 +0000

    Add Question resouces and test

--- lib/random_data.rb
+++ lib/random_data.rb
@@ -23,4 +23,14 @@ module RandomData
      letters.shuffle!
      letters[0,rand(3..8)].join
    end
+   
+   def self.random_question
+     strings = []
+     rand(3..8).times do
+       strings << random_word
+     end
+ 
+     sentence = strings.join(" ")
+     sentence.capitalize << "????"
+   end
 end
