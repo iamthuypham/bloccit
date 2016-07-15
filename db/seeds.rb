cd4bfcc0c38a740886449b9e6ec4504b969300ea
Thuy Pham <tpham045@hotmail.com>
2016-07-15 16:00:11 +0000

    Vote: add MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,16 @@ require 'random_data'
  topics = Topic.all
 
  50.times do
-     Post.create!(
+ post = Post.create!(
      user:   users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
+   
+ post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ # #13
+ rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
  end
  posts = Post.all
  
@@ -63,3 +67,4 @@ require 'random_data'
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
  puts "#{Question.count} questions created"
+ puts "#{Vote.count} votes created"
