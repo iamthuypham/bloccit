99f448f4ceaad6f16f59d1a8d080679eb6c0f3f3
Thuy Pham <tpham045@hotmail.com>
2016-07-16 23:43:08 +0000

    Auto-favorite after create and Favorite Mailer for new post

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -13,4 +13,19 @@ class FavoriteMailer < ApplicationMailer
      mail(to: user.email, subject: "From Thuy: testing my app sending email. New comment on #{post.title}")
      
   end
+  
+  
+  def new_post(user, topic, post)
+
+     headers["Message-ID"] = "<posts/#{post.id}@gentle-stream-45974.com>"
+     headers["In-Reply-To"] = "<topic/#{topic.id}@gentle-stream-45974.com>"
+     headers["References"] = "<topic/#{topic.id}@gentle-stream-45974.com>"
+ 
+     @user = user
+     @topic = topic
+     @post = post
+
+     mail(to: user.email, subject: "From Bloccit App: Your New Post #{post.title} Is Created")
+     
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
   has_many :labels, through: :labelings
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  after_create :auto_favorite_own_post
+  after_create :send_new_own_post
   
   default_scope { order('rank DESC') }
   
@@ -30,5 +32,13 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+  
+  def auto_favorite_own_post
+    user.favorites.create(user: user, post: self)
+  end
+  
+  def send_new_own_post
+    FavoriteMailer.new_post(user, topic, self).deliver_now
   end
 end
